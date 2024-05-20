e26c24b5d2356e1ac7f1af92eaee6e55c5aa0b82
KarayaOne <30974631+KarayaOne@users.noreply.github.com>
2024-05-20 17:16:13 +0500

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 import Favicon from "react-favicon";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Favicon url="https://raw.githubusercontent.com/ziaaurrehman/score2admin/main/client/src/assets/favicon.ico" />
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Favicon url="https://raw.githubusercontent.com/ziaaurrehman/score2admin/main/client/src/assets/favicon.ico" />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
