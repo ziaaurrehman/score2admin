c61d2bf70261fe71f111ef1734514d5f8be1d9c0
KarayaOne <30974631+KarayaOne@users.noreply.github.com>
2024-05-20 19:08:09 +0500

    fix re renders

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,6 @@ import Favicon from "react-favicon";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Favicon url="https://raw.githubusercontent.com/ziaaurrehman/score2admin/main/client/src/assets/favicon.ico" />
     <App />
   </BrowserRouter>
 );
